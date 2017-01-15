de7f0094bcc67c891ad8d040f4d57aac5a718d62
tamal <tamal@appscode.com>
2017-01-15 02:00:16 -0800

    Move version to its own file.

--- main.go
+++ main.go
@@ -10,36 +10,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-	Version         string
-	VersionStrategy string
-	Os              string
-	Arch            string
-	CommitHash      string
-	GitBranch       string
-	GitTag          string
-	CommitTimestamp string
-	BuildTimestamp  string
-	BuildHost       string
-	BuildHostOs     string
-	BuildHostArch   string
-)
-
-func init() {
-	v.Version.Version = Version
-	v.Version.VersionStrategy = VersionStrategy
-	v.Version.Os = Os
-	v.Version.Arch = Arch
-	v.Version.CommitHash = CommitHash
-	v.Version.GitBranch = GitBranch
-	v.Version.GitTag = GitTag
-	v.Version.CommitTimestamp = CommitTimestamp
-	v.Version.BuildTimestamp = BuildTimestamp
-	v.Version.BuildHost = BuildHost
-	v.Version.BuildHostOs = BuildHostOs
-	v.Version.BuildHostArch = BuildHostArch
-}
-
 func main() {
 	rootCmd := &cobra.Command{
 		Use:   "baler [command]",
